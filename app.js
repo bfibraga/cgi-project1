d1da146029f5969f1b445114205aa63d6992a8d0
Bruno Braga <58065472+bfibraga@users.noreply.github.com>
2021-10-18 17:56:55 +0100

    Implemented Varying Color in Vertex Shader

--- app.js
+++ app.js
@@ -4,27 +4,51 @@ import * as MV from '../../libs/MV.js'
 /** @type {WebGLRenderingContext} */
 let gl;
 var program;
-let count = 0;
+
+let widthloc;
+let heightloc;
+let thetaloc;
+
 const table_width = 3.0;
 let table_height;
+let table_center = MV.vec2(table_width/2.0,table_height/2.0);
 
-function addCharge(canvas){
-    canvas.addEventListener("click", function (event) {
-        const ref = MV.vec2((canvas.width/2.0), (canvas.height/2.0))
-        const x = event.offsetX - canvas.width/2;
-        const y = canvas.height/2 - event.offsetY;
-        console.log("Click at (" + x + ", " + y + ")");
-        if (event.shiftKey){
-            //Add Negative Charge
-            //TODO:
-            console.log("Negative Charge added")
-        } else {
-            //Add Positive Charge
-            //TODO:
-            console.log("Positive Charge added")
+let vertices = [];
+let charges = [];
+const MAX_CHARGES = 20;
+const ANGULAR_SPEED = 2;
+
+const key = {
+    "Space": " "
+};
+
+function toggleMagneticField(){
+    document.addEventListener("keydown", function(event){
+        switch(event.key){
+            case key["Space"]:
+                //Toggle Magnetic Field
+                break;
         }
-        
     });
+}
+
+function handleInputCanvas(event, canvas){
+    const x = (table_width * event.offsetX) / canvas.width - table_width/2;
+    const y = (-table_height * event.offsetY) / canvas.height + table_height/2;
+    console.log("Click at (" + x + ", " + y + ")");
+    gl.bufferSubData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"]  + charges.length * MV.sizeof["vec2"],  MV.flatten(MV.vec2(x,y)));
+    charges.push(MV.vec2(x, y));
+    if (event.shiftKey){
+        //Add Negative Charge
+        //TODO:
+        //DEBUGGING
+        console.log("Negative Charge added")
+    } else {
+        //Add Positive Charge
+        //TODO:
+        //DEBUGGING
+        console.log("Positive Charge added")
+    }
 
 }
 
@@ -32,15 +56,18 @@ function animate(time)
 {
     window.requestAnimationFrame(animate);
 
+    time = 0;
+
+    gl.uniform1f(thetaloc, time);
+
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.useProgram(program);
-    var widthloc = gl.getUniformLocation(program, "table_width");
     gl.uniform1f(widthloc, table_width);
-    var heightloc = gl.getUniformLocation(program, "table_height");
     gl.uniform1f(heightloc, table_height);
-    gl.drawArrays(gl.POINTS, 0, count);
-}
+    gl.drawArrays(gl.POINTS, 0, vertices.length);
+    gl.drawArrays(gl.POINTS, vertices.length, charges.length);
+}   
 
 function setup(shaders)
 {
@@ -49,24 +76,28 @@ function setup(shaders)
 
     program = UTILS.buildProgramFromSources(gl, shaders["shader1.vert"], shaders["shader1.frag"]);
 
+    widthloc = gl.getUniformLocation(program, "table_width");
+    heightloc = gl.getUniformLocation(program, "table_height");
+    thetaloc = gl.getUniformLocation(program, "utheta");
+
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
     table_height = (canvas.height * table_width) / canvas.width;
-
-    const vertices = [];
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    
     const grid_spacing = 0.05;
 
-    for(let x = -table_width/2; x <= table_width/2; x += grid_spacing) {
-        for(let y = -table_height/2; y <= table_height/2; y += grid_spacing) {
+    const table_offset = grid_spacing/2.0;
+    for(let x = -table_width/2 + table_offset; x <= table_width/2; x += grid_spacing) {
+        for(let y = -table_height/2 + table_offset; y <= table_height/2; y += grid_spacing) {
             vertices.push(MV.vec2(x, y));
-            count++;
         }
     }
 
     const pBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, MV.flatten(vertices), gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices.length*MV.sizeof["vec2"] + MAX_CHARGES*MV.sizeof["vec2"] + (MAX_CHARGES + vertices.length)*MV.sizeof['vec4'], gl.STATIC_DRAW)
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, MV.flatten(vertices));
 
     const vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
@@ -79,10 +110,17 @@ function setup(shaders)
         gl.viewport(0, 0, canvas.width, canvas.height);
     });
 
-    addCharge(canvas);
+    canvas.addEventListener("click", function (event) {
+        handleInputCanvas(event, canvas);
+    });
 
+    /*for(let i=0; i<MAX_CHARGES; i++) {
+        const uPosition = gl.getUniformLocation(program, "uPosition[" + i + "]");
+        gl.uniform2fv(uPosition, MV.flatten(vertices[i]));
+    }*/
+
+    toggleMagneticField();
     
-    gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     
     window.requestAnimationFrame(animate);
--- shaders/shader1.vert
+++ shaders/shader1.vert
@@ -1,9 +1,63 @@
 attribute vec4 vPosition;
 uniform float table_width;
 uniform float table_height;
+uniform float utheta;
+varying vec4 fColor;
+uniform vec2 uTable_center;
+
+const int MAX_CHARGES=20;
+uniform vec2 uPosition[MAX_CHARGES];
+uniform float uChages[MAX_CHARGES];
+
+#define TWOPI 6.28318530718
+
+// convert angle to hue; returns RGB
+// colors corresponding to (angle mod TWOPI):
+// 0=red, PI/2=yellow-green, PI=cyan, -PI/2=purple
+vec3 angle_to_hue(float angle) {
+  angle /= TWOPI;
+  return clamp((abs(fract(angle+vec3(3.0, 2.0, 1.0)/3.0)*6.0-3.0)-1.0), 0.0, 1.0);
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+vec4 colorize(vec2 f)
+{
+    float a = atan(f.y, f.x);
+    return vec4(angle_to_hue(a-TWOPI), 1.);
+}
+
+vec4 rotate(vec4 p, float angle) {
+    float s = sin(utheta);
+    float c = cos(utheta);
+
+    return vec4((-s * p.y + c * p.x),(s * p.x + c * p.y),0.0,1.0);
+}
+
+float dotProdVec2(vec4 v1, vec4 v2) {
+    return v1.x*v2.x + v1.y*v2.y;
+}
+
+float norm(vec4 v){
+    float res;
+    res += v.x*v.x;
+    res += v.y*v.y;
+    return sqrt(res);
+}
+
+float angleBetween(vec4 v1, vec4 v2){
+    return acos(dotProdVec2(v1, v2) / (norm(v1) * norm(v2)));
+}
 
 void main()
 {
+    gl_Position = rotate(vPosition, utheta);
     gl_PointSize = 4.0;
-    gl_Position = vPosition / vec4(table_width/2.0, table_height/2.0, 1.0, 1.0);
+    gl_Position /= vec4(table_width/2.0, table_height/2.0, 1.0, 1.0);
+    fColor = colorize(vec2(gl_Position.x,gl_Position.y));
 }
