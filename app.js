c451f3d75c09f9daa681a0956da1ba0cec5fab63
Gonçalo Vinagre Martins <57198766+GoncaloZen@users.noreply.github.com>
2021-10-21 23:14:02 +0100

    Problema nos Buffers
    Quando se comenta o buffer das charges, a grid aparece bem, mas se se descomenta já fica mal e os pontos da grid só aparecem quando se clica

--- app.js
+++ app.js
@@ -9,31 +9,38 @@ const table_width = 3.0;
 let table_height;
 
 //Eletric Field
+var pBuffer;
 var eletric_point = [];
-var eletric_new_point = [];
+const grid_spacing = 0.05;
+
 
 //Uniform Locations
-var widthloc;
-var heightloc;
-var rotationloc;
-var translationloc;
+var widthloc_charges;
+var heightloc_charges;
+var widthloc_electric;
+var heightloc_electric;
 var nchargesloc;
-var eletric_loc = [];
-var thetaloc;
 
 //Charges
+var cBuffer;
 let charges = [];
 var n_charges = 0;
-const MAX = 20;
-var rotation = [0.0,1.0];
-const ANGULAR_SPEED = 0.025;
-var theta = 0;
+const MAX = 40;
+const ANGULAR_SPEED = 0.03;
 const NEGATIVE = -1.0;
 const POSITIVE = 1.0;
-const POSITIVE_COLOR = [0.0,1.0,0.0,1.0];
-const NEGATIVE_COLOR = [1.0,0.0,0.0,1.0];
-var colors = [];
+let isActive = true;
 
+function changePos(){
+    let s = Math.sin(ANGULAR_SPEED);
+    let c = Math.cos(ANGULAR_SPEED);
+    for(let i = 0; i < charges.length; i++){
+        let rotation = MV.vec3(charges[i][0] * c - charges[i][2] * charges[i][1] * s, charges[i][1] * c + charges[i][2] * charges[i][0] * s, charges[i][2]);
+        charges[i] = rotation;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0,  MV.flatten(charges));
+}
 
 function animate()
 {
@@ -41,19 +48,19 @@ function animate()
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
+    gl.useProgram(program_eletric_field);
+    gl.uniform1f(widthloc_electric, table_width);
+    gl.uniform1f(heightloc_electric, table_height);
+    gl.drawArrays(gl.LINES, 0, eletric_point.length);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer); 
     gl.useProgram(program_charges);
-
-    theta += ANGULAR_SPEED;
-    /*rotation[0] = Math.sin(theta);
-    rotation[1] = Math.cos(theta);
-    gl.uniform2fv(rotationloc, rotation);*/
-    gl.uniform1f(thetaloc, theta);
-    gl.uniform2fv(translationloc, [0.0,0.0]);
-
-    gl.uniform1f(widthloc, table_width);
-    gl.uniform1f(heightloc, table_height);
-    gl.drawArrays(gl.POINTS, 0, eletric_point.length);
-    gl.drawArrays(gl.POINTS, eletric_point.length, charges.length);
+    changePos();
+    gl.uniform1f(widthloc_charges, table_width);
+    gl.uniform1f(heightloc_charges, table_height);
+    if(isActive)
+        gl.drawArrays(gl.POINTS, 0, charges.length);
 }
 
 function setup(shaders)
@@ -66,30 +73,35 @@ function setup(shaders)
 
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
     table_height = (canvas.height * table_width) / canvas.width;
     
-    widthloc = gl.getUniformLocation(program_charges, "table_width");
-    heightloc = gl.getUniformLocation(program_charges, "table_height");
-    rotationloc = gl.getUniformLocation(program_charges, "uRotation");
-    translationloc = gl.getUniformLocation(program_charges, "uTranslation");
+    widthloc_charges = gl.getUniformLocation(program_charges, "table_width");
+    heightloc_charges = gl.getUniformLocation(program_charges, "table_height");
     nchargesloc = gl.getUniformLocation(program_charges, "uSize");
-    thetaloc = gl.getUniformLocation(program_charges, "uTheta");
-    colors = [POSITIVE_COLOR, NEGATIVE_COLOR];
     gl.uniform1i(nchargesloc, 0);
 
-    const grid_spacing = 0.05;
-
-    /*for(let x = -table_width/2; x <= table_width/2; x += grid_spacing) {
+    widthloc_electric = gl.getUniformLocation(program_eletric_field, "table_width");
+    heightloc_electric = gl.getUniformLocation(program_eletric_field, "table_height");
+    
+    for(let x = -table_width/2; x <= table_width/2; x += grid_spacing) {
         for(let y = -table_height/2; y <= table_height/2; y += grid_spacing) {
-            eletric_point.push(MV.vec2(x, y));
+            eletric_point.push(MV.vec2(x,y));
         }
-    }*/
+    }
 
-    const pBuffer = gl.createBuffer();
+    pBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, eletric_new_point.length*MV.sizeof["vec2"] + eletric_point.length*MV.sizeof["vec2"] + MAX*MV.sizeof["vec3"], gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, eletric_point.length*MV.sizeof["vec2"], gl.STATIC_DRAW)
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, MV.flatten(eletric_point));
+
+    const vPositionElectric = gl.getAttribLocation(program_eletric_field, "vPosition");
+    gl.vertexAttribPointer(vPositionElectric, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPositionElectric);
+
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, MAX*MV.sizeof["vec3"], gl.STATIC_DRAW)
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, MV.flatten(charges));
 
     const vPosition = gl.getAttribLocation(program_charges, "vPosition");
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
@@ -103,26 +115,34 @@ function setup(shaders)
     });
 
     canvas.addEventListener("click", function(event) {
-        // Start by getting x and y coordinates inside the canvas element
         const x = (table_width * event.offsetX) / canvas.width - table_width/2;
         const y = (-table_height * event.offsetY) / canvas.height + table_height/2;
         console.log("Click at (" + x + ", " + y + ")");
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
         if (event.shiftKey){
             var new_charge = MV.vec3(x,y,NEGATIVE);
-            gl.bufferSubData(gl.ARRAY_BUFFER, eletric_point.length * MV.sizeof["vec2"]  + charges.length * MV.sizeof["vec3"],  MV.flatten(new_charge));
+            gl.bufferSubData(gl.ARRAY_BUFFER, charges.length * MV.sizeof["vec3"],  MV.flatten(new_charge));
             charges.push(new_charge);
             console.log("Negative Charge added");
         } else {  
             var new_charge = MV.vec3(x,y,POSITIVE);
-            gl.bufferSubData(gl.ARRAY_BUFFER, eletric_point.length * MV.sizeof["vec2"]  + charges.length * MV.sizeof["vec3"],  MV.flatten(new_charge));
+            gl.bufferSubData(gl.ARRAY_BUFFER, charges.length * MV.sizeof["vec3"],  MV.flatten(new_charge));
             charges.push(new_charge);
             console.log("Positive Charge added");
         }
-        const uPosition = gl.getUniformLocation(program_charges, "uPosition[" + n_charges + "]");
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
+        /*const uPosition = gl.getUniformLocation(program_eletric_field, "uPosition[" + n_charges + "]");
         gl.uniform2fv(uPosition, MV.flatten(new_charge));
         n_charges += 1;
         gl.uniform1i(nchargesloc, n_charges);
+        console.log(charges);*/
     });
+
+    window.addEventListener("keydown", function(event){
+        if(event.keyCode == 32)
+            isActive = !isActive;
+    })
       
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
--- shaders/shader_charges.vert
+++ shaders/shader_charges.vert
@@ -3,32 +3,15 @@ attribute vec4 vPosition;
 uniform float table_width;
 uniform float table_height;
 
-uniform float uTheta;
-
 varying vec4 fColor;
-
-uniform vec2 uTranslation;
-uniform vec2 uRotation;
 
 void main()
 {
     vec2 resolution = vec2(table_width, table_height);
 
-    float s = sin(uTheta);
-    float c = cos(uTheta);
-
-    //Calculate rotation
-    vec2 rotation = vec2(
-        vPosition.z * (vPosition.x * c - vPosition.y * s),
-        vPosition.y * c + vPosition.x * s
-    );
-
-    //Calculate the translation
-    vec4 position = vec4(rotation.x + uTranslation.x, rotation.y + uTranslation.y, 0.0, 1.0);
-
-    gl_PointSize = 10.0;
+    gl_PointSize = 5.0;
     vec4 conversion = vec4(resolution.x/2.0, resolution.y/2.0, 1.0, 1.0);
-    gl_Position = position / conversion;
+    gl_Position = vPosition / conversion;
 
     if(vPosition.z > 0.0)
         fColor = vec4(0.0,1.0,0.0,1.0);
